3a1c29b6e77f63f49358f8f6f65f5b5a060859b1
Nelma da Silva Barros <nelmasb@gmail.com>
2020-08-31 21:09:18 -0300

    Primeiro commit - Adicionando 4 slides

---
+++ README.md
@@ -0,0 +1,10 @@
+## App projeto Organizze
+
+>> Aplicativo para Controle de finanças
+
+Este aplicativo faz parte do curso - Udemy Jamilton Damasceno
+Foi desenvolvido para estudo da plataforma Android
+
+Dependências:
+- Firebase
+- https://github.com/heinrichreimer/material-intro
---
+++ app/src/main/java/com/example/aprenda/organizze/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.aprenda.organizze;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.heinrichreimersoftware.materialintro.app.IntroActivity;
+import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
+
+public class MainActivity extends IntroActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //Comentada para exibir os slides add
+//        setContentView(R.layout.activity_main);
+
+
+        //hide visible button slide
+        setButtonBackVisible(false);
+        setButtonNextVisible(false);
+
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_1)
+                .build()
+        );
+
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_2)
+                .build()
+        );
+
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_3)
+                .build()
+        );
+
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_4)
+                .build()
+        );
+    }
+}
