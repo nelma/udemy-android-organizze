ffead45547c92fe09ef3aadc12e729610496efb9
Nelma da Silva Barros <nelmasb@gmail.com>
2020-09-01 09:41:48 -0300

    Tela login e cadastro

--- app/src/main/java/com/example/aprenda/organizze/MainActivity.java
+++ app/src/main/java/com/example/aprenda/organizze/MainActivity.java
@@ -2,8 +2,12 @@ package com.example.aprenda.organizze;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import com.example.aprenda.organizze.activity.CadastroActivity;
+import com.example.aprenda.organizze.activity.LoginActivity;
 import com.heinrichreimersoftware.materialintro.app.IntroActivity;
 import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
 
@@ -44,5 +48,19 @@ public class MainActivity extends IntroActivity {
                 .fragment(R.layout.intro_4)
                 .build()
         );
+
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_cadastro)
+                .build()
+        );
+    }
+
+    public void btnEntrar(View view) {
+        startActivity(new Intent(this, LoginActivity.class));
+    }
+
+    public void btnCadastrar(View view) {
+        startActivity(new Intent(this, CadastroActivity.class));
     }
 }
